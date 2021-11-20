10e308b634258ab4e70c7903e278e9c702e1f06e
henrycallejas <henry.callejas@gmail.com>
2021-11-19 19:20:24 -0600

    Added: New maven Project with spring boot

---
+++ src/main/java/online/restadvidor/restadvidor/RestadvidorApplication.java
@@ -0,0 +1,13 @@
+package online.restadvidor.restadvidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestadvidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestadvidorApplication.class, args);
+	}
+
+}
